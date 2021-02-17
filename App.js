4267c795841b6932b68b59beda7662d3e3e257d3
Shamoil Arsiwala <shamoilarsiwala16@gmail.com>
2021-02-17 10:56:15 +0530

    setting up

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import Logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={Logo} />
+      <StatusBar style='auto' />
     </View>
   );
 }
