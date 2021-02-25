033ecea830ecdd1412e841d1d5d566c915cab621
Shamoil Arsiwala <shamoilarsiwala16@gmail.com>
2021-02-25 16:01:48 +0530

    added linear gradient

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, TouchableWithoutFeedback } from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
 const Item = ({ data: { image: uri, temp, country, city, timestamp, editable }, onPress }) => {
@@ -13,9 +14,25 @@ const Item = ({ data: { image: uri, temp, country, city, timestamp, editable }, 
         <Image source={{ uri: newuri }} style={{ height: 184 }} />
       </TouchableWithoutFeedback>
       <View style={styles.topLeftDate}>
+        <LinearGradient
+          colors={['rgba(0,0,0,0.3)', 'transparent']}
+          style={{
+            position: 'absolute',
+            top: -92,
+            left: -92,
+            width: 189,
+            height: 189,
+            borderRadius: 80,
+            transform: [{ rotateZ: '-45deg' }],
+          }}
+        />
         <Text style={[styles.text, { fontSize: 14, textAlign: 'center' }]}>{months[date.getMonth()]}</Text>
         <Text style={[styles.text, { fontSize: 24 }]}>{date.getDate()}</Text>
       </View>
+      <LinearGradient
+        style={{ position: 'absolute', bottom: 0, left: 0, right: 0, height: 37 }}
+        colors={['transparent', 'rgba(0,0,0,0.2)']}
+      />
       <View style={styles.bottomLeftLocation}>
         <Feather name='map-pin' size={15} color='#fff' />
         <Text style={[styles.text, { fontSize: 12, marginLeft: 5 }]}>
