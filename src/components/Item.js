00d390c9b1dc5961529b4d6aa5c49538dd14901b
Shamoil Arsiwala <shamoilarsiwala16@gmail.com>
2021-02-25 18:58:32 +0530

    fixed gradient

--- src/components/Item.js
+++ src/components/Item.js
@@ -13,19 +13,21 @@ const Item = ({ data: { image: uri, temp, country, city, timestamp, editable }, 
       <TouchableWithoutFeedback onPress={onPress}>
         <Image source={{ uri: newuri }} style={{ height: 184 }} />
       </TouchableWithoutFeedback>
-      <View style={styles.topLeftDate}>
+      <View style={styles.circularGradientContainer}>
         <LinearGradient
-          colors={['rgba(0,0,0,0.3)', 'transparent']}
+          colors={['rgba(108, 108, 108, 0.90)', 'rgba(0, 0, 0, 0)']}
           style={{
             position: 'absolute',
-            top: -92,
-            left: -92,
+            top: -100,
+            left: -100,
             width: 189,
             height: 189,
-            borderRadius: 80,
-            transform: [{ rotateZ: '-45deg' }],
+            borderRadius: 180,
+            transform: [{ rotateZ: '-40deg' }],
           }}
         />
+      </View>
+      <View style={styles.topLeftDate}>
         <Text style={[styles.text, { fontSize: 14, textAlign: 'center' }]}>{months[date.getMonth()]}</Text>
         <Text style={[styles.text, { fontSize: 24 }]}>{date.getDate()}</Text>
       </View>
@@ -50,6 +52,14 @@ const Item = ({ data: { image: uri, temp, country, city, timestamp, editable }, 
 export default Item;
 
 const styles = StyleSheet.create({
+  circularGradientContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: 100,
+    height: 100,
+    overflow: 'hidden',
+  },
   topLeftDate: { position: 'absolute', left: 7, top: 5 },
   text: {
     fontFamily: 'InterSemiBold',
